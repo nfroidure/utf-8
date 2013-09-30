056bf579d22f0b0b1cd495c74e18b880b7592de1
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-09-30 11:47:50 +0200

    Added encoding + tests

--- src/UTF8.js
+++ src/UTF8.js
@@ -8,26 +8,30 @@
 	var UTF8={
 		// UTF8 decoding functions
 		'getCharLength': function(theByte) {
-			// 4 bytes encoded char
+			// 4 bytes encoded char (mask 11110000)
 			if(0xF0==(theByte&0xF0)) {
 				return 4;
-			// 3 bytes encoded char
+			// 3 bytes encoded char (mask 11100000)
 			} else if(0xE0==(theByte&0xE0)) {
 				return 3;
-			// 2 bytes encoded char
+			// 2 bytes encoded char (mask 11000000)
 			} else if(0xC0==(theByte&0xC0)) {
 				return 2;
 			// 1 bytes encoded char
-			} else {
+			} else if(theByte==(theByte&0x7F)) {
 				return 1;
 			}
+			return 0;
 		},
 		'getCharCode': function(bytes, byteOffset, charLength) {
-			if(charLength)
 			var charCode=0, mask='';
 			byteOffset=byteOffset||0;
 			// Retrieve charLength if not given
 			charLength=charLength||UTF8.getCharLength(bytes[byteOffset]);
+			if(charLength==0) {
+				throw new Error(bytes[byteOffset].toString(2)+' is not a significative'
+					+' byte (offset:'+byteOffset+').');
+			}
 			// Return byte value if charlength is 1
 			if(1===charLength) {
 				return bytes[byteOffset];
@@ -44,6 +48,11 @@
 			charCode+=(bytes[byteOffset]&parseInt(mask,2))<<((--charLength)*6);
 			// Reading the next bytes
 			while(charLength) {
+				if(0x80!==bytes[byteOffset+1]&0x80
+					||0x40===bytes[byteOffset+1]&0x40) {
+					throw Error('Index '+(byteOffset+1)+': Next bytes of encoded char'
+						+' must begin with a "10" bit sequence.');
+				}
 				charCode+=((bytes[++byteOffset]&0x3F)<<((--charLength)*6));
 			}
 			return charCode;
@@ -52,7 +61,7 @@
 			var charLength, chars=[];
 			byteOffset=byteOffset|0;
 			byteLength=('number' === typeof byteLength?byteLength:
-				bytes.byteLength||bytes.Length);
+				bytes.byteLength||bytes.length);
 			for(; byteOffset<byteLength; byteOffset++) {
 				charLength=UTF8.getCharLength(bytes[byteOffset]);
 				if(byteOffset+charLength>byteLength) {
@@ -69,6 +78,48 @@
 				byteOffset+=charLength-1;
 			}
 			return chars.join('');
+		},
+		// UTF8 encoding functions
+		'getBytesForCharCode':function(charCode) {
+			for(var i=0; i<4; i++) {
+				if(charCode<Math.pow(2,7+(i*6))) {
+					return i+1;
+				}
+			}
+			throw new Error('CharCode '+charCode+' cannot be encoded with UTF8.');
+		},
+		'setBytesFromCharCode':function(charCode, bytes, byteOffset, neededBytes) {
+			charCode=charCode|0;
+			bytes=bytes||[];
+			byteOffset=byteOffset|0;
+			neededBytes=neededBytes||UTF8.getBytesForCharCode(charCode);
+			// Setting the charCode as it to bytes if the byte length is 1
+			if(1==neededBytes) {
+				bytes[byteOffset]=charCode;
+			} else {
+				// Computing the first byte
+				bytes[byteOffset++]=(parseInt('1111'.slice(0,neededBytes),2)<<8-neededBytes)
+					+(charCode>>>((--neededBytes)*6));
+				// Computing next bytes
+				for(;neededBytes>0;) {
+					bytes[byteOffset++]=((charCode>>>((--neededBytes)*6))&0x3F)|0x80;
+				}
+			}
+			return bytes;
+		},
+		'setBytesFromString': function(string, bytes, byteOffset, byteLength) {
+			string=string||'';
+			bytes=bytes||[];
+			byteOffset=byteOffset|0;
+			byteLength=('number' === typeof byteLength?byteLength:
+				bytes.byteLength||Infinity);
+			for(var i=0, j=string.length; i<j; i++) {
+				var neededBytes=UTF8.getBytesForCharCode(string[i].charCodeAt(0));
+				UTF8.setBytesFromCharCode(string[i].charCodeAt(0),
+					bytes, byteOffset, neededBytes);
+				byteOffset+=neededBytes;
+			}
+			return bytes;
 		}
 	};
 
